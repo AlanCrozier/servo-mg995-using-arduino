cd62b694e5a9aad453ef149cae354c634c781560
alvincrozier20 <alvincrozierjhs@gmail.com>
2026-02-18 09:33:46 +0530

    add library of servo

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include<Servo.h>
+
+Servo myservo;
+
+int trg_pin = 9;
+int ech_pin = 10;
+
+
+void setup() {
+    pinMode(trg_pin,OUTPUT);
+    pinMode(ech_pin,INPUT);
+    myservo.attach(5);
+    Serial.begin(9600);
+}
+
+void loop() {
+    digitalWrite(trg_pin,HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trg_pin,LOW);
+    delayMicroseconds(10);
+    digitalWrite(trg_pin,HIGH);
+    long duration = pulseIn(ech_pin,HIGH);
+    float distance = duration*(0.0343/2);
+    if(distance<=15){
+        myservo.write(180);
+
+    }else{
+        myservo.write(0);
+    }
+    Serial.print("Distance");
+    Serial.print(distance);
+    Serial.println("CM");
+    delay(10);
+}
